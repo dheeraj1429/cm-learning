5737d6b9f1ff4c31f397a1cddcf192066a4a3ec6
dheeraj1429 <dheerajsingh1429@gmail.com>
2023-07-23 20:46:19 +0530

    learning layouts, protected router, next auth, session

---
+++ src/app/(site)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+   return (
+      <div className="p-2 text-center">
+         <h1 className="text-gray-800 font-medium text-xl">
+            Hello from the nextjs app
+         </h1>
+      </div>
+   );
+}
--- src/app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-   return <div className="p-2"></div>;
-}
